05421003b2c88764118affa6a3d1b33b9009465c
d.padalka <d.padalka@notamedia.ru>
2024-07-23 09:38:02 +0200

    Убрал отступ

--- app/Services/TimerService.php
+++ app/Services/TimerService.php
@@ -1,6 +1,5 @@
 <?php
 
-
 namespace App\Services;
 
 
@@ -14,7 +13,7 @@ class TimerService
     public static function stopTracking()
     {
         $timer = Timer::getUserStarted()->first();
-        if ( !$timer ) {
+        if (!$timer) {
             return;
         }
 
