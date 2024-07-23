abeba44560a3e66511a2347ddbf955c0475b0a04
d.padalka <d.padalka@notamedia.ru>
2024-07-23 10:35:35 +0200

    Локализация, хелпер-синглтон

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -25,7 +25,9 @@ class Task extends Model
 
     public function getTotalTimeAttribute()
     {
-        return TimeHelper::secondsToFormatTime($this->seconds_spent ?: 0);
+        $timeHelper = app()->make(TimeHelper::class);
+
+        return $timeHelper->secondsToFormatTime($this->seconds_spent ?: 0);
     }
 
     public function addSeconds(int $seconds = 0)
