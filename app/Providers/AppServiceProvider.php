4e69afd79702de1ac2b33f50b1ac089d3c62d955
d.padalka <d.padalka@notamedia.ru>
2024-08-08 19:08:59 +0200

    Удалил лишние классы

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,10 +2,8 @@
 
 namespace App\Providers;
 
-use App\Models\Task;
 use Illuminate\Http\Request;
 use Illuminate\Support\ServiceProvider;
-use App\Services\Helpers\TimeHelper;
 use Illuminate\Contracts\Foundation\Application;
 use App\Services\TimerService;
 
